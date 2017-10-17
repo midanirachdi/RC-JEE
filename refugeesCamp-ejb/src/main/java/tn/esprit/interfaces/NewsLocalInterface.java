c2f0bfc0de36b579f530db732cefbf8e069584cf
Lukenjevi <salimbhassine@gmail.com>
2017-10-17 14:55:40 +0100

    Implementing Interfaces

---
+++ refugeesCamp-ejb/src/main/java/tn/esprit/interfaces/NewsLocalInterface.java
@@ -0,0 +1,26 @@
+package tn.esprit.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import tn.esprit.entities.News;
+
+/*
+* author: Salim Ben Hassine
+*/
+@Local
+public interface NewsLocalInterface {
+
+	boolean add(News news);
+
+	boolean update(News news);
+
+	List<News> findAll();
+
+	News findById(int id);
+
+	boolean delete(News news);
+
+}
+
