9eca9aa838d54d0cc50df25e1ef6bdd3b30af709
Khalil Fakhfekh <khalil.fakhfekh@esprit.tn>
2017-10-21 15:02:22 +0100

    donation service creatted

---
+++ refugeesCamp-ejb/src/main/java/tn/esprit/interfaces/DonationRemoteInterface.java
@@ -0,0 +1,17 @@
+package tn.esprit.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import tn.esprit.entities.Camp;
+import tn.esprit.entities.Donation;
+@Remote
+public interface DonationRemoteInterface {
+	boolean add(Donation d);
+	boolean delete(Donation d);
+	boolean update(Donation d);
+	List<Donation> findAll();
+	List<Donation> findByCamp(Camp c);
+	Donation findById(String id);
+}
---
+++ refugeesCamp-ejb/src/main/java/tn/esprit/services/DonationService.java
@@ -0,0 +1,53 @@
+package tn.esprit.services;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import tn.esprit.entities.Camp;
+import tn.esprit.entities.Donation;
+import tn.esprit.interfaces.DonationRemoteInterface;
+
+@Stateless
+public class DonationService implements DonationRemoteInterface{
+	
+	@PersistenceContext(unitName = "refugeesCamp-ejb")
+	EntityManager em;
+	@Override
+	public boolean add(Donation d) {
+		em.persist(d);
+		return true;
+	}
+
+	@Override
+	public boolean delete(Donation d) {
+		em.remove(em.merge(d));
+		return true;
+	}
+
+	@Override
+	public List<Donation> findAll() {
+		String requete = "SELECT d FROM Donation d";
+		return em.createQuery(requete,Donation.class).getResultList();
+	}
+
+	@Override
+	public List<Donation> findByCamp(Camp c) {
+		String requete = "SELECT d FROM Donation d WHERE d.to_camp ="+c.getId();
+		return em.createQuery(requete,Donation.class).getResultList();
+	}
+
+	@Override
+	public Donation findById(String id) {
+		return em.find(Donation.class, id);
+	}
+
+	@Override
+	public boolean update(Donation d) {
+		em.merge(d);
+		return true;
+	}
+	
+}
