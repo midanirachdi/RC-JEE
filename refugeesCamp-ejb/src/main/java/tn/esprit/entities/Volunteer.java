66fc28fbc7a4d505357be52ced4eb2ffc6b7b204
Khalil Fakhfekh <khalil.fakhfekh@esprit.tn>
2017-11-09 01:04:12 +0100

    entity event created

--- refugeesCamp-ejb/src/main/java/tn/esprit/entities/Volunteer.java
+++ refugeesCamp-ejb/src/main/java/tn/esprit/entities/Volunteer.java
@@ -1,7 +1,13 @@
 package tn.esprit.entities;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
@@ -10,6 +16,17 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @JsonTypeName(value = "Volunteer")
 public class Volunteer extends User {
 	
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "volunter_event", joinColumns = @JoinColumn(name = "volunteer_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "event_id", referencedColumnName = "id"))
+	private List<Event> events;
+	
 	public Volunteer(){}
 
+	public List<Event> getEvents() {
+		return events;
+	}
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+	
 }
